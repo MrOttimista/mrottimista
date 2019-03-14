d977ecb0e82dd1baf9d4aba88364a60504692313
adel <adel.mohamed855@gmail.com>
2019-03-14 22:09:03 +0200

    Updates

---
+++ precache-manifest.1f1b0e6379a1ef4a68b8007b74c374c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mrottimista/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2c580b4618afd7aa80b1",
+    "url": "/mrottimista/static/js/runtime~main.be10cfae.js"
+  },
+  {
+    "revision": "a362a1fd1a0013685d1e",
+    "url": "/mrottimista/static/js/main.d3233640.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/mrottimista/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "a362a1fd1a0013685d1e",
+    "url": "/mrottimista/static/css/main.b54b1fd5.chunk.css"
+  },
+  {
+    "revision": "10b5916cc3bb03c6b94d82019d07c629",
+    "url": "/mrottimista/index.html"
+  }
+];
